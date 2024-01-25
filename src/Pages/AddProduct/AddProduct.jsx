2011104bba2f5bbe23fb6c070f9eac2c7ad4df2e
Hanifc56 <abuhanif007sd@gmail.com>
2024-01-26 04:28:06 +0600

    Added a product

--- src/Pages/AddProduct/AddProduct.jsx
+++ src/Pages/AddProduct/AddProduct.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../Shared/Navbar";
 import { useState } from "react";
 import { Switch } from "@headlessui/react";
+import Swal from "sweetalert2";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -16,9 +17,38 @@ const AddProduct = () => {
     const type = form.type.value;
     const price = form.price.value;
     const rateing = form.rateing.value;
+    const discription = form.discription.value;
 
-    const newProduct = { name, brand, photo, type, price, rateing };
+    const newProduct = {
+      name,
+      brand,
+      photo,
+      type,
+      price,
+      rateing,
+      discription,
+    };
     console.log(newProduct);
+    // send data to the server
+    fetch("http://localhost:5000/products", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newProduct),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Success!",
+            text: "Added Successfully",
+            icon: "success",
+            confirmButtonText: "COOL",
+          });
+        }
+      });
   };
 
   return (
@@ -144,6 +174,23 @@ const AddProduct = () => {
                   name="rateing"
                   id="rateing"
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="discription"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Short Discription
+              </label>
+              <div className="mt-2.5">
+                <textarea
+                  name="discription"
+                  id="discription"
+                  rows={4}
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  defaultValue={""}
                 />
               </div>
             </div>
