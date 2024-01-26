d7323c047254079ceb7b368c51e9f3c3c2ceb2b5
Hanifc56 <abuhanif007sd@gmail.com>
2024-01-26 17:58:52 +0600

    Links updated

--- src/Pages/Home/BrandsSlider.jsx
+++ src/Pages/Home/BrandsSlider.jsx
@@ -9,7 +9,7 @@ const BrandsSlider = () => {
         </h2>
         <Marquee pauseOnHover={true} speed={100}>
           <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-6">
-            <Link to="">
+            <Link to="/brand/Levis">
               <img
                 className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
                 src="https://i.ibb.co/ZmkgNk9/levi-logo-4-82250bd000e1-mobile.jpg"
@@ -18,41 +18,52 @@ const BrandsSlider = () => {
                 height={48}
               />
             </Link>
-            <img
-              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-              src="https://i.ibb.co/7vmG0wj/1512076803-93-Nike-mobile.jpg"
-              alt="Reform"
-              width={158}
-              height={48}
-            />
-            <img
-              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-              src="https://i.ibb.co/F7Pq9tg/Adidas-Logo-1991.jpg"
-              alt="Tuple"
-              width={158}
-              height={48}
-            />
-            <img
-              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-              src="https://i.ibb.co/xXXY101/e632a23a8796a6f2357958adfbb8dcfc.jpg"
-              alt="SavvyCal"
-              width={158}
-              height={48}
-            />
-            <img
-              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-              src="https://i.ibb.co/c2Vyw2K/2560px-Zara-Logo-svg.png"
-              alt="Statamic"
-              width={158}
-              height={48}
-            />
-            <img
-              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-              src="https://i.ibb.co/cLD76pJ/download.png"
-              alt="Statamic"
-              width={158}
-              height={48}
-            />
+
+            <Link to="/brand/Nike">
+              <img
+                className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                src="https://i.ibb.co/7vmG0wj/1512076803-93-Nike-mobile.jpg"
+                alt="Reform"
+                width={158}
+                height={48}
+              />
+            </Link>
+            <Link to="/brand/Adidas">
+              <img
+                className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                src="https://i.ibb.co/F7Pq9tg/Adidas-Logo-1991.jpg"
+                alt="Tuple"
+                width={158}
+                height={48}
+              />
+            </Link>
+            <Link to="/brand/Gucci">
+              <img
+                className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                src="https://i.ibb.co/xXXY101/e632a23a8796a6f2357958adfbb8dcfc.jpg"
+                alt="SavvyCal"
+                width={158}
+                height={48}
+              />
+            </Link>
+            <Link to="/brand/Zara">
+              <img
+                className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                src="https://i.ibb.co/c2Vyw2K/2560px-Zara-Logo-svg.png"
+                alt="Statamic"
+                width={158}
+                height={48}
+              />
+            </Link>
+            <Link to="/brand/H&M">
+              <img
+                className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                src="https://i.ibb.co/cLD76pJ/download.png"
+                alt="Statamic"
+                width={158}
+                height={48}
+              />
+            </Link>
           </div>
         </Marquee>
       </div>
