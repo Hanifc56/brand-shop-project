74da0d465adefdc129c1d7df2e68d8075c0dc8fe
Hanifc56 <abuhanif007sd@gmail.com>
2024-01-26 19:07:55 +0600

    Carusol added

--- src/Pages/Shared/Slider.jsx
+++ src/Pages/Shared/Slider.jsx
@@ -1,9 +1,64 @@
-import React from "react";
-
 const Slider = () => {
   return (
     <div>
-      <h1>slider goes here</h1>
+      <div className="carousel w-full rounded-lg mb-6">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img
+            src="https://i.ibb.co/DbtRkV5/taylor-smith-a-DZ5-YIued-Qg-unsplash.jpg"
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide2" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide2" className="carousel-item relative w-full">
+          <img
+            src="https://i.ibb.co/6rd2cYJ/daniel-storek-JM-q-KEd1-GMI-unsplash.jpg"
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide3" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide3" className="carousel-item relative w-full">
+          <img
+            src="https://i.ibb.co/kgWf5Sw/tamara-bellis-n-On-T17l-KYz8-unsplash.jpg"
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide4" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide4" className="carousel-item relative w-full">
+          <img
+            src="https://i.ibb.co/MP9jsCD/james-ree-Zme-Ftu11-Hpc-unsplash.jpg"
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide1" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
