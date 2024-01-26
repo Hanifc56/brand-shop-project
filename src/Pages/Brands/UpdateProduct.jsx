dc31dc877fcad7b240c8da5d494c333693acb07b
Hanifc56 <abuhanif007sd@gmail.com>
2024-01-26 18:47:47 +0600

    Update product page Added

--- src/Pages/Brands/UpdateProduct.jsx
+++ src/Pages/Brands/UpdateProduct.jsx
@@ -6,7 +6,7 @@ import Swal from "sweetalert2";
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
-const updateProduct = () => {
+const UpdateProduct = () => {
   const [agreed, setAgreed] = useState(false);
   const handleUpdateProduct = (e) => {
     e.preventDefault();
@@ -50,7 +50,6 @@ const updateProduct = () => {
         }
       });
   };
-
   return (
     <div>
       <div className="isolate bg-white px-6  sm:pb-32 lg:px-8">
@@ -70,10 +69,10 @@ const updateProduct = () => {
 
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Add Your Product
+            Update Your Product
           </h2>
           <p className="mt-2 text-lg leading-8 text-gray-600">
-            Give detailed Info about your product
+            Give detailed Info about your product to Update
           </p>
         </div>
         <form
@@ -229,7 +228,7 @@ const updateProduct = () => {
               type="submit"
               className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Add Product
+              Update
             </button>
           </div>
         </form>
@@ -238,4 +237,4 @@ const updateProduct = () => {
   );
 };
 
-export default updateProduct;
+export default UpdateProduct;
