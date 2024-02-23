b63dff2335db3c89ad07f7c8cb3c8d287237b83c
Hanifc56 <abuhanif007sd@gmail.com>
2024-02-24 01:16:12 +0600

    push in local

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA-jM1u1i8OoCboNfUmYLgfKEF4BZJOY3I",
-  authDomain: "brand-shop-with-db.firebaseapp.com",
-  projectId: "brand-shop-with-db",
-  storageBucket: "brand-shop-with-db.appspot.com",
-  messagingSenderId: "590278032925",
-  appId: "1:590278032925:web:42078cc3176fa4bf4410f4",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
